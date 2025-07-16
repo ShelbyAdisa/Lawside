75f1b67d8d41049087467eaa8b4a9be4fe25b671
ShelbyAdisa <sheladisaa@gmail.com>
2025-07-16 17:08:29 +0300

    Allowed Hosts

--- backend/legal_platform/settings.py
+++ backend/legal_platform/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-1n-43xf8#jvh)9*e+d36a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', 'lawside.onrender.com']
 # Custom User Model
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
